90013bd292489e85ec2bae24e5bf92560a7ac5dc
Estefanny1 <estefanny.cruz201@tectijuana.edu.mx>
2025-01-09 10:05:13 -0800

    '092enero2025'

--- app/components/ReportTemplate/Conclusions/Botton-Nervius.jsx
+++ app/components/ReportTemplate/Conclusions/Botton-Nervius.jsx
@@ -10,11 +10,18 @@ export function NerviusButton({ title, value, displayText }) {
 
   // Clase condicional para que el botón seleccionado sea visible y los demás transparentes
   const classnames = 'cursor-pointer p-1 text-sm text-white transition-colors duration-300 ease-in ' + 
-  (conclusions.find(cl => cl.value === value) ? 'bg-[#c44900]' : 'bg-transparent') + ' rounded-[50px] z-50 relative';
+    (selectedButton === value ? 'bg-[#c44900]' : 'bg-transparent') + ' rounded-[50px] z-50 relative';
 
- // Función para actualizar las conclusiones
- function handleClick() {
-  updateConclusions({ title, value });
+  // Función para actualizar las conclusiones y mostrar o esconder el botón seleccionado
+  function handleClick() {
+    if (selectedButton === value) {
+      // Si ya está seleccionado, deseleccionarlo (hacerlo invisible)
+      setSelectedButton(null);
+    } else {
+      // Si no está seleccionado, seleccionarlo (hacerlo visible)
+      setSelectedButton(value);
+    }
+    updateConclusions({ title, value });
   }
 
   return (
@@ -23,3 +30,10 @@ export function NerviusButton({ title, value, displayText }) {
     </div>
   );
 }
+
+
+
+
+
+
+

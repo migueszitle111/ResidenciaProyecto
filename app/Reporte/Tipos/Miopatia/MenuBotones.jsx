e863557926d6b66094f7ee63b5d10baf48cf3b7e
SergioEstrada14 <sergio.estrada201@tectijuana.edu.mx>
2025-01-10 14:34:22 -0800

    Cambios Generales 3 10-01-25

--- app/Reporte/Tipos/Miopatia/MenuBotones.jsx
+++ app/Reporte/Tipos/Miopatia/MenuBotones.jsx
@@ -151,7 +151,7 @@ const StepA = ({ handleNextStep }) => {
           </h1>
 
           <div onClick={ handleNextStep }>
-            <ConclusionButton value='evolucion_aguda' title='MIOPATIA AGUDA' displayText={'AGUDA'}/>
+            <ConclusionButton value='evolucion_aguda' title='MIOPATIA AGUDA' displayText={'AGUDAA'}/>
             <ConclusionButton value='evolucion_subaguda' title='MIOPATIA SUBAGUDA' displayText={'SUBAGUDA'}/>
             <ConclusionButton value='evolucion_cronica' title='MIOPATIA CRÓNICA' displayText={'CRÓNICA'}/>
             <ConclusionButton value='evolucion_antigua' title='MIOPATIA ANTIGUA' displayText={'ANTIGUA'}/>

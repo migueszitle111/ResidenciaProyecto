87c3ae5852587ba33abc1d9c478d3fb2935f7898
migueszitle111 <miguel.zitle193@tectijuana.edu.mx>
2025-05-28 01:08:05 -0700

    cambios

--- app/Evento/page.js
+++ app/Evento/page.js
@@ -1,33 +1,23 @@
+// app/evento/page.jsx   (o donde lo tengas ubicado)
 "use client";
-import Overhead from "../components/Overhead";
+
+import Overhead     from "../components/Overhead";
 import OverheadMenu from "../components/OverheadMenu";
-import SubMenu from "../components/Submenu";
 import "./Style.css";
-import Footer from "../components/Footer";
-import PdfUploader from "./PdfUploader";
-import CongresoNavbar from "./CongresoNavbar";
-import CursoNavbar from "./CursoNavbar";
-import CursoList from "./CursoList";
-import CongresoList from "./CongresoList";
-import DiplomadosNavbar from "./DiplomaNavbar";
-import DiplomadoList from "./DiplomadoList";
-import { useSession } from "next-auth/react";
-import CardsList from "../components/CardsList";
+import Footer       from "../components/Footer";
+import CardsList    from "../components/CardsList";
 
 export default function Evento() {
-  // Obtiene la sesión actual del usuario
-  const { data: session } = useSession();
-  // Verifica si el usuario es administrador
-  const isAdmin = session && session.user && session.user.roles === "admin";
-
   return (
     <div className="Conteiner w-full">
       <Overhead />
       <OverheadMenu />
-      <div className="Conteiner">
+
+      {/* margen superior para separar un poco del menú */}
+      <div className="Conteiner mt-8">
         <CardsList />
       </div>
-        
+
       <Footer />
     </div>
   );

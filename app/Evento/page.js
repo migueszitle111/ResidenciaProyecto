cfcae059d159e27dfce1430584a2bb4f4461efed
migueszitle111 <miguel.zitle193@tectijuana.edu.mx>
2025-05-28 01:16:36 -0700

    cambios

--- app/Evento/page.js
+++ app/Evento/page.js
@@ -1,23 +1,27 @@
-// app/evento/page.jsx   (o donde lo tengas ubicado)
+// app/evento/page.jsx
 "use client";
 
 import Overhead     from "../components/Overhead";
 import OverheadMenu from "../components/OverheadMenu";
-import "./Style.css";
 import Footer       from "../components/Footer";
 import CardsList    from "../components/CardsList";
+import "./Style.css";
 
 export default function Evento() {
   return (
-    <div className="Conteiner w-full">
+    <div className="flex flex-col min-h-screen w-full">
+      {/* Cabecera fija en la parte superior */}
       <Overhead />
       <OverheadMenu />
 
-      {/* margen superior para separar un poco del menú */}
-      <div className="Conteiner mt-8">
-        <CardsList />
-      </div>
+      {/* Contenido principal que crece y centra el CardsList */}
+      <main className="flex-grow mt-8 flex justify-center px-4">
+        <div className="w-full max-w-4xl">
+          <CardsList />
+        </div>
+      </main>
 
+      {/* Footer pegado al final */}
       <Footer />
     </div>
   );

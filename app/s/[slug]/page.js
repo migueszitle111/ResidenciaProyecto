b202daafe2f5599e868caeae1357c4dfe16a452b
migueszitle111 <miguel.zitle193@tectijuana.edu.mx>
2025-10-04 16:55:45 -0700

    link

--- app/s/[slug]/page.js
+++ app/s/[slug]/page.js
@@ -31,10 +31,6 @@ function iconFor(mime = '', name = '') {
   if (isPdf) {
     return (
       <div className={common} aria-hidden>
-        <svg viewBox="0 0 24 24" className="w-6 h-6 text-slate-600">
-          <path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8z" />
-          <path fill="currentColor" d="M14 2v6h6" className="opacity-40" />
-        </svg>
         <span className="ml-1 text-[10px] font-black tracking-wide" style={{ color: BRAND }}>
           PDF
         </span>

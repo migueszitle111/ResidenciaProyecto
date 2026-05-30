c04d2b28dd252e6b7907e06ffac27066474bc851
migueszitle111 <miguel.zitle193@tectijuana.edu.mx>
2026-05-30 15:26:12 -0700

    banner

--- app/components/BannerPublicitario.js
+++ app/components/BannerPublicitario.js
@@ -5,23 +5,17 @@ export default function BannerPublicitarios() {
   return (
     <div>
       <div className="Conteiner mx-auto">
-      <div className="w-full lg:w-2/3 flex flex-col mx-auto m-5 rounded-tr-lg rounded-bl-lg p-5">
-        <Image
-          src="/assets/LandingPage/Page/LP-16.png"
-          alt="Banner top"
-          width={1200}
-          height={400}
-          className="w-full h-auto object-cover"
-        />
-      </div>
-      <div className="w-full lg:w-2/3 flex flex-col mx-auto m-5 rounded-tr-lg rounded-bl-lg p-5">
-        <Image
-          src="/assets/LandingPage/Page/LP-10.png"
-          alt="Banner top"
-          width={1200}
-          height={400}
-          className="w-full h-auto object-cover"
-        />
+     
+      <div className="w-full lg:w-2/3 flex flex-col mx-auto m-2 rounded-tr-lg rounded-bl-lg p-5">
+        <a href="mailto:lab@medxproapp.com" className="cursor-pointer">
+          <Image
+            src="/assets/LandingPage/Page/LP-10.png"
+            alt="Banner top"
+            width={1200}
+            height={400}
+            className="w-full h-auto object-cover"
+          />
+        </a>
       </div>
       </div>
     </div>
